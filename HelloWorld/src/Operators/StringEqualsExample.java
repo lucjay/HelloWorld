b418ac9945043b36623463654ccb2623f22daa6b
User <User@YD01-04>
2019-12-18 12:50:44 +0900

    123

--- HelloWorld/src/Operators/StringEqualsExample.java
+++ HelloWorld/src/Operators/StringEqualsExample.java
@@ -1,16 +1,18 @@
 package Operators;
-class Student{
+
+class Student {
 	int age;
 	String name;
 }
+
 public class StringEqualsExample {
 	public static void main(String[] args) {
 		String str1 = "이빈우";
 		String str2 = "이빈우";
 		String str3 = new String("이빈우");
-		
+
 		Student stu1 = new Student();
-		
+
 		System.out.println(stu1);
 		System.out.println(str1 == str2);
 		System.out.println(str2 == str3);
@@ -24,6 +26,7 @@ public class StringEqualsExample {
 		} else {
 			System.out.println("다른 내용입니다.");
 		}
+
 	}
 
 }
