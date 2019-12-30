7b9e74922848c682e62550937e70cf0e60dee638
GIGA H81M <GIGA H81M@119.201.77.223>
2019-12-31 01:05:21 +0900

    h

--- HelloWorld/src/Classes/FriendExe.java
+++ HelloWorld/src/Classes/FriendExe.java
@@ -4,49 +4,53 @@ import java.util.Scanner;
 
 public class FriendExe {
 	public static void main(String[] args) {
-		Friend [] feArray = new Friend[10];
-		Scanner sc = new Scanner(System.in);
+		Friend[] feArray = new Friend[10];
+		Scanner scanner = new Scanner(System.in);
 
-		
 		boolean run = true;
 		while (run) {
 			System.out.println("-------------------------------");
 			System.out.println("1.입력 | 2.조회 | 3.리스트 | 4.종료 ");
 			System.out.println("-------------------------------");
 			System.out.println("선택> ");
-			
-			int sNo = sc.nextInt();
-			sc.nextLine();
+
+			int sNo = scanner.nextInt();
+			scanner.nextLine();
 			if (sNo == 1) {
-				(unifrined);
+				unifrined();
 			} else if (sNo == 2) {
-				(sch);
+				sch();
 			} else if (sNo == 3) {
-				(list);
+				list();
 			} else if (sNo == 4) {
-				run = false;			
-	}
+				run = false;
+			}
 		}
 		System.out.println("프로그램 종료");
-		
+
 	}
+
+	static Scanner scanner;
 
 	private static void unifrined() {
 		FriendExe fe = new FriendExe();
-		System.out.println("------선택");
-		System.out.println("대학교");
-		System.out.println("회사");
-		System.out.println("그외");
-		sNo = sc.nextInt();
-		sc.nextLine();
-		if (sNo == 1) {
-			System.out.println();
-		}
+		System.out.println("-----------------------");
+		System.out.println("1.대학교 | 2.회사 | 3.그외 ");
+		System.out.println("-----------------------");
+		System.out.println("선택> ");
+		String name = scanner.nextLine();
+		fe.set
 
-		for (int i = 0; i < feAray.length; i++) {
-			if (feAray[i] == null) {
-				feAray[i] = fe;
-				break;
+		scanner.nextLine();
+		int sn;
+		if (sn == 1) {
+			System.out.println();
+
+			for (int i = 0; i < feArray.length; i++) {
+				if (feArray[i] == null) {
+					feArray[i] = fe;
+					break;
+				}
 			}
 		}
 	}
